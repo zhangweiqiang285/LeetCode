af23ee9d055c36d4307379832c0597f9614c490f
zhangweiqiang285 <zhangweiqiang285@yeah.net>
2019-04-02 23:54:33 +0800

    Update solution.java

--- 000. 两数之和/solution.java
+++ 000. 两数之和/solution.java
@@ -1,7 +1,15 @@
 class solution
 {
-	public static void main(String[] main)
-	{
-		System.out.println("11");
-	}
+    public int[] twoSum(int[] nums, int target) {
+
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            for(int j = 0; j < nums.length - 1; j++)
+            {
+                if((target == nums[i] + nums[j]) && (i != j))
+                    return new int[]{i,j};
+            }
+        }
+        return null;
+    }
 }
