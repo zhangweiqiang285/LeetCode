c3eaf15ee53fc018819e42b54a67ee96d5968b16
zhangweiqiang285 <zhangweiqiang285@yeah.net>
2019-04-08 21:30:59 +0800

    Create solution.java

---
+++ 035. 搜索插入位置/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int flag = 0;
+        for(int i = 0; i < nums.length; i++)
+        {  
+            if(nums[i] == target)
+            {
+                return i;
+            }
+            else
+            {
+                if(nums[i] < target)
+                {
+                    flag++;
+                }
+            }
+        }
+        return flag;
+        
+    }
+}
