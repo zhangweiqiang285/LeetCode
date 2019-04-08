99886224bbbf6621f466c2eeddcd5747cead85a8
zhangweiqiang285 <zhangweiqiang285@yeah.net>
2019-04-08 23:42:02 +0800

    Create solution.java

---
+++ 070. 爬楼梯/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        if (n==2) return 2;
+        int[] step = new int[n+1];
+        step[1] = 1;
+        step[2] = 2;
+        for (int i=3;i<=n;i++){
+            step[i] = step[i-1]+step[i-2];
+        }
+        return step[n];
+    }
+}
