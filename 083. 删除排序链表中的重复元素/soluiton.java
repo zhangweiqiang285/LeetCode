eb3cadd5fc062b83711e2984f7585a8849f0bc3d
zhangweiqiang <zhangweiqiang285@yeah.net>
2019-05-05 21:05:16 +0800

    Update soluiton.java

--- 083. 删除排序链表中的重复元素/soluiton.java
+++ 083. 删除排序链表中的重复元素/soluiton.java
@@ -5,23 +5,37 @@
  *     ListNode next;
  *     ListNode(int x) { val = x; }
  * }
+ *
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ *
+ * 示例 1:
+ * 输入: 1->1->2
+ * 输出: 1->2
+ *
+ * 示例 2:
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ * 
+ * 由于不需要删除头结点，所以用dummy等于head就可以，然后循环判断值是否相等，相等的话跳过
  */
+
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null) return head;
 
-        ListNode index = head;
+        ListNode dummy = head;
 
 
-        while (index != null && index.next != null)
+        while (dummy != null && dummy.next != null)
         {
-            if(index.val == index.next.val)
+            if(dummy.val == dummy.next.val)
             {
-                index.next = index.next.next;
+                dummy.next = dummy.next.next;
             }
             else
             {
-                index = index.next;
+                dummy = dummy.next;
             }
         }
 
